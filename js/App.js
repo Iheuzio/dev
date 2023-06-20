4f252577fbf78618bf8ae02533c3269b9a6cae5c
Christopher <christopherbartos@protonmail.com>
2023-06-20 00:53:00 -0400

    removed unused code, will work on it later

--- js/App.js
+++ js/App.js
@@ -16,17 +16,3 @@ fetch(`https://api.github.com/repos/${owner}/${repo}`)
         localStorage.setItem('watchers', watchers);
     })
     .catch(error => console.error(error));
-
-    const url = 'https://github.com/iheuzio';
-
-fetch(url)
-  .then(response => response.text())
-  .then(html => {
-    const parser = new DOMParser();
-    const doc = parser.parseFromString(html, 'text/html');
-    const pinnedItems = doc.querySelectorAll('div.pinned-item-list-item-content');
-    console.log(pinnedItems);
-  })
-  .catch(error => {
-    console.log(error);
-  });
