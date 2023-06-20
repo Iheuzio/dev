6a2457a88ba46e11fcc0886801bc47f5ce9ace85
Christopher <christopherbartos@protonmail.com>
2023-06-20 00:07:15 -0400

    fix(mobile-devices): now mobile devices are supported

--- js/App.js
+++ js/App.js
@@ -1,5 +1,6 @@
 const owner = 'Iheuzio';
 const repo = 'TickerPricePrediction';
+const pinned = "d-flex flex-wrap list-style-none gutter-condensed mb-2 js-pinned-items-reorder-list"
 
 fetch(`https://api.github.com/repos/${owner}/${repo}`)
     .then(response => response.json())
@@ -15,3 +16,17 @@ fetch(`https://api.github.com/repos/${owner}/${repo}`)
         localStorage.setItem('watchers', watchers);
     })
     .catch(error => console.error(error));
+
+    const url = 'https://github.com/iheuzio';
+
+fetch(url)
+  .then(response => response.text())
+  .then(html => {
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(html, 'text/html');
+    const pinnedItems = doc.querySelectorAll('div.pinned-item-list-item-content');
+    console.log(pinnedItems);
+  })
+  .catch(error => {
+    console.log(error);
+  });
