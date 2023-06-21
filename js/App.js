d9aebe4288c793b8bb8de99a0d57e7566fa242fe
Christopher <christopherbartos@protonmail.com>
2023-06-21 14:37:25 -0400

    Added new project

--- js/App.js
+++ js/App.js
@@ -1,18 +1,32 @@
 const owner = 'Iheuzio';
-const repo = 'TickerPricePrediction';
-const pinned = "d-flex flex-wrap list-style-none gutter-condensed mb-2 js-pinned-items-reorder-list"
+const repo = ['TickerPricePrediction','gpt-contextfiles'];
+const count = repo.length;
+const repoData = [];
 
-fetch(`https://api.github.com/repos/${owner}/${repo}`)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-        const stars = data.stargazers_count;
-        const forks = data.forks_count;
-        const watchers = data.subscribers_count;
+for(let i=0; i<count; i++){
+    fetch(`https://api.github.com/repos/${owner}/${repo[i]}`)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            const stars = data.stargazers_count;
+            const forks = data.forks_count;
+            const watchers = data.subscribers_count;
 
-        // Store the variables in localStorage
-        localStorage.setItem('stars', stars);
-        localStorage.setItem('forks', forks);
-        localStorage.setItem('watchers', watchers);
-    })
-    .catch(error => console.error(error));
+            // Store the variables in an object
+            const dataObj = {
+                repoName: repo[i],
+                stars: stars,
+                forks: forks,
+                watchers: watchers
+            };
+
+            // Push the object to the repoData array
+            repoData.push(dataObj);
+
+            // Store the variables in localStorage
+            localStorage.setItem(`${repo[i]}_stars`, stars);
+            localStorage.setItem(`${repo[i]}_forks`, forks);
+            localStorage.setItem(`${repo[i]}_watchers`, watchers);
+        })        .catch(error => console.error(error));
+
+}
