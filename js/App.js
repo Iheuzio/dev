313c41aff47c7241c5d3cdda01860b9cc3e82dd8
Christopher Bartos <christopher.bartos@dawsoncollege.qc.ca>
2023-12-26 14:36:06 -0500

    fix(local-storage)
    Fetch using the github api and store the data directly, rather than storing in the localstorage

--- js/App.js
+++ js/App.js
@@ -12,20 +12,9 @@ for(let i=0; i<count; i++){
             const forks = data.forks_count;
             const watchers = data.subscribers_count;
 
-            // Store the variables in an object
-            const dataObj = {
-                repoName: repo[i],
-                stars: stars,
-                forks: forks,
-                watchers: watchers
-            };
+            document.getElementById(`${repo[i]}_stars`).innerHTML = stars == undefined ? 0 : stars;
+            document.getElementById(`${repo[i]}_forks`).innerHTML = forks == undefined ? 0 : forks;
+            document.getElementById(`${repo[i]}_watchers`).innerHTML = watchers == undefined ? 0 : watchers;
 
-            // Push the object to the repoData array
-            repoData.push(dataObj);
-
-            // Store the variables in localStorage
-            localStorage.setItem(`${repo[i]}_stars`, stars);
-            localStorage.setItem(`${repo[i]}_forks`, forks);
-            localStorage.setItem(`${repo[i]}_watchers`, watchers);
-        })        .catch(error => console.error(error));
+        }).catch(error => console.error(error));
 }
